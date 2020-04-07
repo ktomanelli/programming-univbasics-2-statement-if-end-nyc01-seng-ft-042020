615af95bb5fcc9029e19360a27c7e20d08ba1e0e
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-07 19:59:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,9 @@
 # Write your solution here
 myTime = (Time.now).to_i
-# if Time.now.select_second%2==0
-#   p"Even!"
-# else
-#   p"odd!"
-# end
+if %2==0
+  p"Even!"
+else
+  p"odd!"
+end
 
 puts myTime
